ff6f69f1c7ec9d44221debfe04d1066f68eec186
Trish <trishajjohnson@gmail.com>
2021-10-21 20:01:45 -0500

    key props

--- part-3/App.js
+++ part-3/App.js
@@ -1,9 +1,9 @@
 function App() {
     return (
       <div>
-        <Person name="Trisha" age={35} hobbies={["playing guitar", "yoga", "coding"]} />
-        <Person name="Harlow" age={6} hobbies={["painting", "riding bicycle", "ballet"]}/>
-        <Person name="Callioppee" age={32} hobbies={["setting bones", "dancing in her underwear", "traveling"]}/>
+        <Person name="Trisha" age={35} hobbies={[{id: 1, hobby: "playing guitar"}, {id: 2, hobby: "yoga"}, {id: 3, hobby: "coding"}]} />
+        <Person name="Harlow" age={6} hobbies={[{id: 1, hobby: "painting"}, {id: 2, hobby: "riding bicycle"}, {id: 3, hobby: "ballet"}]}/>
+        <Person name="Callioppee" age={32} hobbies={[{id: 1, hobby: "setting bones"}, {id: 2, hobby: "dancing in her underwear"}, {id: 3, hobby: "traveling"}]}/>
       </div>
     );
   }
--- part-3/Person.js
+++ part-3/Person.js
@@ -4,7 +4,7 @@ const Person = ({name, age, hobbies}) => {
             <p>Learn some information about this person</p>
             <h3>{age > 18 ? "Please go vote!" : "You must be 18"}</h3>
             <p>{name.length > 8 ? name.substring(0, 7) : name}</p>
-            <ul>{hobbies.map(h => <li>{h}</li>)}</ul>
+            <ul>{hobbies.map(h => <li key={h.id}>{h.hobby}</li>)}</ul>
         </div>
     )
 }
